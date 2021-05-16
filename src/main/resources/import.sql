406e115ea3443cbe15644f29e0cf528e450c896e
Abdul Kareem <abdulkareem.bitguiders@gmail.com>
2021-05-16 00:24:28 -0400

    FICA4 - dataacess layer integration

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+insert into fica_db.fictional_character (fictional_character_id,full_name) values('1','Sir Anthony Absolute (The Rivals)'); 
+insert into fica_db.fictional_character (fictional_character_id,full_name) values('2','Nick Adams (In Our Time and others)'); 
+insert into fica_db.fictional_character (fictional_character_id,full_name) values('3','Molly Bloom (Ulysses)'); 
+insert into fica_db.fictional_character (fictional_character_id,full_name) values('4','Richard Carstone (Bleak House)'); 
+insert into fica_db.fictional_character (fictional_character_id,full_name) values('5','Eliza Doolittle (Pygmalion)'); 
+insert into fica_db.fictional_character (fictional_character_id,full_name) values('6','Finn (Fenian cycle)'); 
+insert into fica_db.fictional_character (fictional_character_id,full_name) values('7','Parson Adams (Joseph Andrews)'); 
+
+insert into fica_db.universe (universe_id,universe_name) values('1','Alagaësia'); 
+insert into fica_db.universe (universe_id,universe_name) values('2','All-World'); 
+insert into fica_db.universe (universe_id,universe_name) values('3','Alliance-Union universe'); 
